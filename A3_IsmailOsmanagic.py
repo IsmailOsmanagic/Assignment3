363c626bee9d2c359bd811f8847a0b52322dbde6
IsmailOsmanagic <iso_osmanagic.at@hotmail.com>
2023-02-12 23:02:51 +0100

    should be correct now

--- A3_IsmailOsmanagic.py
+++ A3_IsmailOsmanagic.py
@@ -1,8 +1,21 @@
+# 1. Example
+
 someNumber = 10
 
-if(someNumber >= 0):
+if(someNumber == 0):
+    print("EQUAL Zero")
+elif(someNumber < 0):
+    print("Bellow Zero")
+else:
+    print("Over Zero")
+
+# 2. Example
+
+someNumber = 10
+
+if(someNumber > 0):
     print("Over Zero")
 elif(someNumber < 0):
     print("Bellow Zero")
 else:
-    print("Zero")
+    print("Equal Zero")
